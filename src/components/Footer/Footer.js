f79ade947b71d89d938babc6bc63f4ba2e09cd74
Renat <renat_ismailov@bk.ru>
2021-09-15 23:54:03 +0300

    feat: Сверстать компонент Footer (+адаптив)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer(props) {
     return(
-        <div>
+        <footer className='footer'>
+            <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+            <div className='footer__content'>
+                <span className='footer__copyright'>&copy; 2020</span>
+                <ul className='footer__links'>
+                    <li className='footer__links-item'><Link to="#" className='footer__link' target='_blank'>Яндекс.Практикум</Link></li>
+                    <li className='footer__links-item'><Link to="#" className='footer__link' target='_blank'>Github</Link></li>
+                    <li className='footer__links-item'><Link to="#" className='footer__link' target='_blank'>Facebook</Link></li>
+                </ul>
+            </div>
 
-        </div>
+        </footer>
     );
 }
 
