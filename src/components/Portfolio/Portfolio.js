382facfe9d6d0f7348899cb72ded190a49dfe077
Renat <renat_ismailov@bk.ru>
2021-09-15 22:58:39 +0300

    feat: Сверстать компонент Portfolio (+адаптив)

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Portfolio.css'
 
 function Portfolio(props) {
     return (
-        <div>
-
-        </div>
+        <section className='portfolio'>
+            <h2 className='portfolio__title'>Портфолио</h2>
+            <div className='portfolio__list'>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Статичный сайт</Link></li>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Адаптивный сайт</Link></li>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Одностраничное приложение</Link></li>
+            </div>
+        </section>
     )
 }
 
