eb306d7c072b010783fb71d82b26f471b0f5908d
Renat <renat_ismailov@bk.ru>
2021-09-15 23:05:43 +0300

    fix: Добавить target blank для ссылок

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,9 +7,9 @@ function Portfolio(props) {
         <section className='portfolio'>
             <h2 className='portfolio__title'>Портфолио</h2>
             <div className='portfolio__list'>
-                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Статичный сайт</Link></li>
-                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Адаптивный сайт</Link></li>
-                <li className='portfolio__item'><Link to='#' className='portfolio__link'>Одностраничное приложение</Link></li>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link' target='_blank'>Статичный сайт</Link></li>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link' target='_blank'>Адаптивный сайт</Link></li>
+                <li className='portfolio__item'><Link to='#' className='portfolio__link' target='_blank'>Одностраничное приложение</Link></li>
             </div>
         </section>
     )
