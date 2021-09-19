7c02c3231907552658d1a2956a4eec8a34794c0f
Renat <renat_ismailov@bk.ru>
2021-09-19 23:52:03 +0300

    fix: Доработать переход по якорю в Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Promo.css';
 import promoImage from '../../images/promo-image.svg'
 
@@ -10,7 +9,7 @@ function Promo(props) {
                 <div className='promo__description'>
                     <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
                     <span className='promo__caption'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</span>
-                    <Link to='#' className='promo__link-button'>Узнать больше</Link>
+                    <a href='#about-project' className='promo__link-button'>Узнать больше</a>
                 </div>
                 <img src={promoImage} className='promo__image' alt='Промо изображение'/>
             </div>
